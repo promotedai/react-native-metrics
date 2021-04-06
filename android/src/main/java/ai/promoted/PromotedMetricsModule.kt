6357d4289d7c664549380efab32c3c13b03cb1e7
Yu-Hong Wang <yu-hong@promoted.ai>
2021-04-06 13:16:50 -0700

    Fix compile on Android

--- android/src/main/java/ai/promoted/PromotedMetricsModule.kt
+++ android/src/main/java/ai/promoted/PromotedMetricsModule.kt
@@ -1,12 +1,6 @@
 package ai.promoted
 
-import com.facebook.react.bridge.ReactApplicationContext
-import com.facebook.react.bridge.ReactContextBaseJavaModule
-import com.facebook.react.bridge.ReactMethod
-import com.facebook.react.bridge.ReadableArray
-import com.facebook.react.bridge.ReadableMap
-import com.facebook.react.bridge.Promise
-import com.facebook.react.bridge.WritableMap
+import com.facebook.react.bridge.*
 
 class PromotedMetricsModule(reactContext: ReactApplicationContext) :
     ReactContextBaseJavaModule(reactContext) {
@@ -20,70 +14,70 @@ class PromotedMetricsModule(reactContext: ReactApplicationContext) :
 
   @ReactMethod
   fun startSessionAndLogSignedOutUser() {}
-  
+
   @ReactMethod @Suppress("UNUSED_PARAMETER")
   fun logImpression(content: ReadableMap) {}
-  
+
   @ReactMethod @Suppress("UNUSED_PARAMETER")
   fun logNavigateAction(screenName: String) {}
-  
+
   @ReactMethod @Suppress("UNUSED_PARAMETER")
   fun logNavigateActionWithContent(screenName: String, content: ReadableMap) {}
-  
+
   @ReactMethod @Suppress("UNUSED_PARAMETER")
   fun logAddToCartAction(item: ReadableMap) {}
-  
+
   @ReactMethod @Suppress("UNUSED_PARAMETER")
   fun logRemoveFromCartAction(item: ReadableMap) {}
-  
+
   @ReactMethod
   fun logCheckoutAction() {}
-  
+
   @ReactMethod @Suppress("UNUSED_PARAMETER")
   fun logPurchaseAction(item: ReadableMap) {}
-  
+
   @ReactMethod @Suppress("UNUSED_PARAMETER")
   fun logShareAction(content: ReadableMap) {}
-  
+
   @ReactMethod @Suppress("UNUSED_PARAMETER")
   fun logLikeAction(content: ReadableMap) {}
-  
+
   @ReactMethod @Suppress("UNUSED_PARAMETER")
   fun logUnlikeAction(content: ReadableMap) {}
-  
+
   @ReactMethod @Suppress("UNUSED_PARAMETER")
   fun logCommentAction(content: ReadableMap) {}
-  
+
   @ReactMethod @Suppress("UNUSED_PARAMETER")
   fun logMakeOfferAction(content: ReadableMap) {}
-  
+
   @ReactMethod @Suppress("UNUSED_PARAMETER")
   fun logAskQuestionAction(content: ReadableMap) {}
-  
+
   @ReactMethod @Suppress("UNUSED_PARAMETER")
   fun logAnswerQuestionAction(content: ReadableMap) {}
-  
+
   @ReactMethod
   fun logCompleteSignInAction() {}
-  
+
   @ReactMethod
   fun logCompleteSignUpAction() {}
-  
+
   @ReactMethod
   fun logAction(name: String) {}
-  
+
   @ReactMethod @Suppress("UNUSED_PARAMETER")
   fun logActionWithType(name: String, type: Int) {}
-  
+
   @ReactMethod @Suppress("UNUSED_PARAMETER")
   fun logActionWithContent(name: String, type: Int, content: ReadableMap) {}
-  
+
   @ReactMethod @Suppress("UNUSED_PARAMETER")
   fun logView(screenName: String) {}
-  
+
   @ReactMethod @Suppress("UNUSED_PARAMETER")
   fun collectionViewDidLoad(collectionViewName: String) {}
-  
+
   @ReactMethod @Suppress("UNUSED_PARAMETER")
   fun collectionViewDidChange(visibleContent: ReadableArray,
                               collectionViewName: String) {}
@@ -91,12 +85,12 @@ class PromotedMetricsModule(reactContext: ReactApplicationContext) :
   @ReactMethod @Suppress("UNUSED_PARAMETER")
   fun collectionViewDidUnmount(collectionViewName: String) {}
 
-  @ReactMethod @Suppress("UNUSED_PARAMETER")
+  @ReactMethod
   fun getLoggingSessionInfo(promise: Promise) {
-    WritableMap result = WritableMap();
-    map.putString("logUserId", "");
-    map.putString("sessionId", "");
-    map.putString("viewId", "");
-    promise(map);
+    var map = Arguments.createMap()
+    map.putString("logUserId", "")
+    map.putString("sessionId", "")
+    map.putString("viewId", "")
+    promise.resolve(map)
   }
 }
