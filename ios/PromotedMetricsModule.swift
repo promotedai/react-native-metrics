bc3447da36a975aba074cd3c9f53c6d93b865120
Y Wang <yu-hong@promoted.ai>
2021-04-28 14:31:41 -0700

    Return immediately in each method when logging is disabled

--- ios/PromotedMetricsModule.swift
+++ ios/PromotedMetricsModule.swift
@@ -38,10 +38,10 @@ public class PromotedMetricsModule: NSObject {
     return MetricsLoggerService.shared
   }
 
-  private var metricsLogger: MetricsLogger {
-    return service.metricsLogger
-  }
+  private var metricsLogger: MetricsLogger? { service.metricsLogger }
   
+  private var config: ClientConfig { service.config }
+    
   private var nameToImpressionLogger: [String: ImpressionLogger]
   
   @objc public override convenience init() {
@@ -100,139 +100,164 @@ public extension PromotedMetricsModule {
   // MARK: - Starting new sessions
   @objc(startSessionAndLogUser:)
   func startSessionAndLogUser(userID: String) {
-    metricsLogger.startSessionAndLogUser(userID: userID)
+    guard config.loggingEnabled else { return }
+    metricsLogger?.startSessionAndLogUser(userID: userID)
   }
   
   @objc(startSessionAndLogSignedOutUser)
   func startSessionAndLogSignedOutUser() {
-    metricsLogger.startSessionAndLogSignedOutUser()
+    guard config.loggingEnabled else { return }
+    metricsLogger?.startSessionAndLogSignedOutUser()
   }
   
   // MARK: - Impressions
   @objc(logImpression:)
   func logImpression(content: ReactNativeDictionary?) {
-    metricsLogger.logImpression(content: contentFor(content))
+    guard config.loggingEnabled else { return }
+    metricsLogger?.logImpression(content: contentFor(content))
   }
   
   // MARK: - Clicks
   @objc(logNavigateAction:)
   func logNavigateAction(screenName: String) {
-    metricsLogger.logNavigateAction(screenName: screenName)
+    guard config.loggingEnabled else { return }
+    metricsLogger?.logNavigateAction(screenName: screenName)
   }
   
   @objc(logNavigateActionWithContent:forContent:)
   func logNavigateAction(screenName: String,
                          forContent content: ReactNativeDictionary?) {
-    metricsLogger.logNavigateAction(screenName: screenName,
-                                    forContent: contentFor(content))
+    guard config.loggingEnabled else { return }
+    metricsLogger?.logNavigateAction(screenName: screenName,
+                                     forContent: contentFor(content))
   }
   
   @objc(logAddToCartAction:)
   func logAddToCartAction(item: ReactNativeDictionary?) {
-    metricsLogger.logAddToCartAction(item: itemFor(item))
+    guard config.loggingEnabled else { return }
+    metricsLogger?.logAddToCartAction(item: itemFor(item))
   }
   
   @objc(logRemoveFromCartAction:)
   func logRemoveFromCartAction(item: ReactNativeDictionary?) {
-    metricsLogger.logRemoveFromCartAction(item: itemFor(item))
+    guard config.loggingEnabled else { return }
+    metricsLogger?.logRemoveFromCartAction(item: itemFor(item))
   }
 
   @objc(logCheckoutAction)
   func logCheckoutAction() {
-    metricsLogger.logCheckoutAction()
+    guard config.loggingEnabled else { return }
+    metricsLogger?.logCheckoutAction()
   }
 
   @objc(logPurchaseAction:)
   func logPurchaseAction(item: ReactNativeDictionary?) {
-    metricsLogger.logPurchaseAction(item: itemFor(item))
+    guard config.loggingEnabled else { return }
+    metricsLogger?.logPurchaseAction(item: itemFor(item))
   }
   
   @objc(logShareAction:)
   func logShareAction(content: ReactNativeDictionary?) {
-    metricsLogger.logShareAction(content: contentFor(content))
+    guard config.loggingEnabled else { return }
+    metricsLogger?.logShareAction(content: contentFor(content))
   }
 
   @objc(logLikeAction:)
   func logLikeAction(content: ReactNativeDictionary?) {
-    metricsLogger.logLikeAction(content: contentFor(content))
+    guard config.loggingEnabled else { return }
+    metricsLogger?.logLikeAction(content: contentFor(content))
   }
   
   @objc(logUnlikeAction:)
   func logUnlikeAction(content: ReactNativeDictionary?) {
-    metricsLogger.logUnlikeAction(content: contentFor(content))
+    guard config.loggingEnabled else { return }
+    metricsLogger?.logUnlikeAction(content: contentFor(content))
   }
   
   @objc(logCommentAction:)
   func logCommentAction(content: ReactNativeDictionary?) {
-    metricsLogger.logCommentAction(content: contentFor(content))
+    guard config.loggingEnabled else { return }
+    metricsLogger?.logCommentAction(content: contentFor(content))
   }
   
   @objc(logMakeOfferAction:)
   func logMakeOfferAction(item: ReactNativeDictionary?) {
-    metricsLogger.logMakeOfferAction(item: itemFor(item))
+    guard config.loggingEnabled else { return }
+    metricsLogger?.logMakeOfferAction(item: itemFor(item))
   }
   
   @objc(logAskQuestionAction:)
   func logAskQuestionAction(content: ReactNativeDictionary?) {
-    metricsLogger.logAskQuestionAction(content: contentFor(content))
+    guard config.loggingEnabled else { return }
+    metricsLogger?.logAskQuestionAction(content: contentFor(content))
   }
   
   @objc(logAnswerQuestionAction:)
   func logAnswerQuestionAction(content: ReactNativeDictionary?) {
-    metricsLogger.logAnswerQuestionAction(content: contentFor(content))
+    guard config.loggingEnabled else { return }
+    metricsLogger?.logAnswerQuestionAction(content: contentFor(content))
   }
   
   @objc(logCompleteSignInAction)
   func logCompleteSignInAction() {
-    metricsLogger.logCompleteSignInAction()
+    guard config.loggingEnabled else { return }
+    metricsLogger?.logCompleteSignInAction()
   }
   
   @objc(logCompleteSignUpAction)
   func logCompleteSignUpAction() {
-    metricsLogger.logCompleteSignUpAction()
+    guard config.loggingEnabled else { return }
+    metricsLogger?.logCompleteSignUpAction()
   }
 
   @objc(logAction:)
   func logAction(name: String) {
-    metricsLogger.logAction(name: name)
+    guard config.loggingEnabled else { return }
+    metricsLogger?.logAction(name: name)
   }
-  
+
   @objc(logActionWithType:type:)
   func logAction(name: String, type: Int) {
+    guard config.loggingEnabled else { return }
     if let actionType = ActionType(rawValue: type) {
-      metricsLogger.logAction(name: name, type: actionType)
+      metricsLogger?.logAction(name: name, type: actionType)
     }
   }
-  
+
   @objc(logActionWithContent:type:content:)
   func logAction(name: String, type: Int, content: ReactNativeDictionary?) {
+    guard config.loggingEnabled else { return }
     if let actionType = ActionType(rawValue: type) {
-      metricsLogger.logAction(name: name, type: actionType, content: contentFor(content))
+      metricsLogger?.logAction(name: name, type: actionType, content: contentFor(content))
     }
   }
-  
+
   // MARK: - Views
   @objc(logViewReady:routeKey:)
   func logViewReady(routeName: String, routeKey: String) {
-    metricsLogger.logViewReady(routeName: routeName, routeKey: routeKey)
+    guard config.loggingEnabled else { return }
+    metricsLogger?.logViewReady(routeName: routeName, routeKey: routeKey)
   }
-  
+
   @objc(logViewReadyWithUseCase:routeKey:useCase:)
   func logViewReady(routeName: String, routeKey: String, useCase: Int) {
+    guard config.loggingEnabled else { return }
     if let u = UseCase(rawValue: useCase) {
-      metricsLogger.logViewReady(routeName: routeName, routeKey: routeKey, useCase: u)
+      metricsLogger?.logViewReady(routeName: routeName, routeKey: routeKey, useCase: u)
     }
   }
-  
+
   @objc(logViewChange:routeKey:)
   func logViewChange(routeName: String, routeKey: String) {
-    metricsLogger.logViewChange(routeName: routeName, routeKey: routeKey)
+    guard config.loggingEnabled else { return }
+    metricsLogger?.logViewChange(routeName: routeName, routeKey: routeKey)
   }
-  
+
   @objc(logViewChangeWithUseCase:routeKey:useCase:)
   func logViewChange(routeName: String, routeKey: String, useCase: Int) {
+    guard config.loggingEnabled else { return }
     if let u = UseCase(rawValue: useCase) {
-      metricsLogger.logViewChange(routeName: routeName, routeKey: routeKey, useCase: u)
+      metricsLogger?.logViewChange(routeName: routeName, routeKey: routeKey, useCase: u)
     }
   }
 }
@@ -248,11 +273,13 @@ public extension PromotedMetricsModule {
   /// - Parameter collectionViewName: Identifier for collection view to track.
   @objc(collectionViewDidLoad:)
   func collectionViewDidLoad(collectionViewName: String) {
+    guard config.loggingEnabled else { return }
     // A load without a previous unmount can be due to a page refresh.
     // Don't recreate the logger in this case.
     if let _ = nameToImpressionLogger[collectionViewName] { return }
-    let logger = service.impressionLogger()
-    nameToImpressionLogger[collectionViewName] = logger
+    if let logger = service.impressionLogger() {
+      nameToImpressionLogger[collectionViewName] = logger
+    }
   }
 
   /// Logs impressions for changed content.
@@ -264,6 +291,7 @@ public extension PromotedMetricsModule {
   ///   - collectionViewName: Identifier for collection view to track.
   @objc(collectionViewDidChange:collectionViewName:)
   func collectionViewDidChange(visibleContent: [AnyObject], collectionViewName: String) {
+    guard config.loggingEnabled else { return }
     guard let logger = nameToImpressionLogger[collectionViewName] else { return }
     var contentList = [Content]()
     for obj in visibleContent {
@@ -280,6 +308,7 @@ public extension PromotedMetricsModule {
   /// - Parameter collectionViewName: Identifier for collection view to track.
   @objc(collectionViewDidUnmount:)
   func collectionViewDidUnmount(collectionViewName: String) {
+    guard config.loggingEnabled else { return }
     if let logger = nameToImpressionLogger.removeValue(forKey: collectionViewName) {
       logger.collectionViewDidHideAllContent()
     }
@@ -290,6 +319,11 @@ public extension PromotedMetricsModule {
 public extension PromotedMetricsModule {
   @objc(getLoggingSessionInfo:rejecter:)
   func loggingSessionInfo(resolver: RCTPromiseResolveBlock, rejecter: RCTPromiseRejectBlock) {
+    guard config.loggingEnabled,
+          let metricsLogger = metricsLogger else {
+      resolver([:])
+      return
+    }
     let sessionInfo = [
       "logUserId": metricsLogger.logUserID,
       "sessionId": metricsLogger.sessionID,
