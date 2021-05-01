4a52b95d6485018a4ce410fbf9627b44f30a0e79
Drew Dennis <dennisdrew@promoted.ai>
2021-04-30 23:03:39 -0400

    Verified consuming android-metrics-sdk package

--- android/src/main/java/ai/promoted/PromotedMetricsModule.kt
+++ android/src/main/java/ai/promoted/PromotedMetricsModule.kt
@@ -1,89 +1,148 @@
 package ai.promoted
 
+import android.app.Application
 import com.facebook.react.bridge.*
 
-class PromotedMetricsModule(reactContext: ReactApplicationContext) :
-    ReactContextBaseJavaModule(reactContext) {
+class PromotedMetricsModule(
+  application: Application,
+  reactContext: ReactApplicationContext
+) :
+  ReactContextBaseJavaModule(reactContext) {
+
+  init {
+    // TODO - pass in custom configuration
+    PromotedAi.initialize(application) {
+
+    }
+  }
 
   override fun getName(): String {
     return "PromotedMetrics"
   }
 
-  @ReactMethod @Suppress("UNUSED_PARAMETER")
-  fun startSessionAndLogUser(userId: String) {}
+  @ReactMethod
+  @Suppress("UNUSED_PARAMETER")
+  fun startSessionAndLogUser(userId: String) {
+    PromotedAi.startSession(userId)
+  }
 
   @ReactMethod
-  fun startSessionAndLogSignedOutUser() {}
-
-  @ReactMethod @Suppress("UNUSED_PARAMETER")
-  fun logImpression(content: ReadableMap) {}
-
-  @ReactMethod @Suppress("UNUSED_PARAMETER")
-  fun logNavigateAction(screenName: String) {}
-
-  @ReactMethod @Suppress("UNUSED_PARAMETER")
-  fun logNavigateActionWithContent(screenName: String, content: ReadableMap) {}
-
-  @ReactMethod @Suppress("UNUSED_PARAMETER")
-  fun logAddToCartAction(item: ReadableMap) {}
-
-  @ReactMethod @Suppress("UNUSED_PARAMETER")
-  fun logRemoveFromCartAction(item: ReadableMap) {}
+  fun startSessionAndLogSignedOutUser() {
+    PromotedAi.startSession()
+  }
 
   @ReactMethod
-  fun logCheckoutAction() {}
-
-  @ReactMethod @Suppress("UNUSED_PARAMETER")
-  fun logPurchaseAction(item: ReadableMap) {}
-
-  @ReactMethod @Suppress("UNUSED_PARAMETER")
-  fun logShareAction(content: ReadableMap) {}
-
-  @ReactMethod @Suppress("UNUSED_PARAMETER")
-  fun logLikeAction(content: ReadableMap) {}
-
-  @ReactMethod @Suppress("UNUSED_PARAMETER")
-  fun logUnlikeAction(content: ReadableMap) {}
-
-  @ReactMethod @Suppress("UNUSED_PARAMETER")
-  fun logCommentAction(content: ReadableMap) {}
-
-  @ReactMethod @Suppress("UNUSED_PARAMETER")
-  fun logMakeOfferAction(content: ReadableMap) {}
-
-  @ReactMethod @Suppress("UNUSED_PARAMETER")
-  fun logAskQuestionAction(content: ReadableMap) {}
-
-  @ReactMethod @Suppress("UNUSED_PARAMETER")
-  fun logAnswerQuestionAction(content: ReadableMap) {}
+  @Suppress("UNUSED_PARAMETER")
+  fun logImpression(content: ReadableMap) {
+  }
 
   @ReactMethod
-  fun logCompleteSignInAction() {}
+  @Suppress("UNUSED_PARAMETER")
+  fun logNavigateAction(screenName: String) {
+  }
 
   @ReactMethod
-  fun logCompleteSignUpAction() {}
+  @Suppress("UNUSED_PARAMETER")
+  fun logNavigateActionWithContent(screenName: String, content: ReadableMap) {
+  }
 
-  @ReactMethod @Suppress("UNUSED_PARAMETER")
-  fun logAction(name: String) {}
+  @ReactMethod
+  @Suppress("UNUSED_PARAMETER")
+  fun logAddToCartAction(item: ReadableMap) {
+  }
 
-  @ReactMethod @Suppress("UNUSED_PARAMETER")
-  fun logActionWithType(name: String, type: Int) {}
+  @ReactMethod
+  @Suppress("UNUSED_PARAMETER")
+  fun logRemoveFromCartAction(item: ReadableMap) {
+  }
 
-  @ReactMethod @Suppress("UNUSED_PARAMETER")
-  fun logActionWithContent(name: String, type: Int, content: ReadableMap) {}
+  @ReactMethod
+  fun logCheckoutAction() {
+  }
 
-  @ReactMethod @Suppress("UNUSED_PARAMETER")
-  fun logView(screenName: String) {}
+  @ReactMethod
+  @Suppress("UNUSED_PARAMETER")
+  fun logPurchaseAction(item: ReadableMap) {
+  }
 
-  @ReactMethod @Suppress("UNUSED_PARAMETER")
-  fun collectionViewDidLoad(collectionViewName: String) {}
+  @ReactMethod
+  @Suppress("UNUSED_PARAMETER")
+  fun logShareAction(content: ReadableMap) {
+  }
 
-  @ReactMethod @Suppress("UNUSED_PARAMETER")
+  @ReactMethod
+  @Suppress("UNUSED_PARAMETER")
+  fun logLikeAction(content: ReadableMap) {
+  }
+
+  @ReactMethod
+  @Suppress("UNUSED_PARAMETER")
+  fun logUnlikeAction(content: ReadableMap) {
+  }
+
+  @ReactMethod
+  @Suppress("UNUSED_PARAMETER")
+  fun logCommentAction(content: ReadableMap) {
+  }
+
+  @ReactMethod
+  @Suppress("UNUSED_PARAMETER")
+  fun logMakeOfferAction(content: ReadableMap) {
+  }
+
+  @ReactMethod
+  @Suppress("UNUSED_PARAMETER")
+  fun logAskQuestionAction(content: ReadableMap) {
+  }
+
+  @ReactMethod
+  @Suppress("UNUSED_PARAMETER")
+  fun logAnswerQuestionAction(content: ReadableMap) {
+  }
+
+  @ReactMethod
+  fun logCompleteSignInAction() {
+  }
+
+  @ReactMethod
+  fun logCompleteSignUpAction() {
+  }
+
+  @ReactMethod
+  @Suppress("UNUSED_PARAMETER")
+  fun logAction(name: String) {
+  }
+
+  @ReactMethod
+  @Suppress("UNUSED_PARAMETER")
+  fun logActionWithType(name: String, type: Int) {
+  }
+
+  @ReactMethod
+  @Suppress("UNUSED_PARAMETER")
+  fun logActionWithContent(name: String, type: Int, content: ReadableMap) {
+  }
+
+  @ReactMethod
+  @Suppress("UNUSED_PARAMETER")
+  fun logView(screenName: String) {
+  }
+
+  @ReactMethod
+  @Suppress("UNUSED_PARAMETER")
+  fun collectionViewDidLoad(collectionViewName: String) {
+  }
+
+  @ReactMethod
+  @Suppress("UNUSED_PARAMETER")
   fun collectionViewDidChange(visibleContent: ReadableArray,
-                              collectionViewName: String) {}
+                              collectionViewName: String) {
+  }
 
-  @ReactMethod @Suppress("UNUSED_PARAMETER")
-  fun collectionViewDidUnmount(collectionViewName: String) {}
+  @ReactMethod
+  @Suppress("UNUSED_PARAMETER")
+  fun collectionViewDidUnmount(collectionViewName: String) {
+  }
 
   @ReactMethod
   fun getLoggingSessionInfo(promise: Promise) {
