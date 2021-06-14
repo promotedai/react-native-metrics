33ebbb3de9b07514483217bee868e06bf9dba127
Y Wang <yu-hong@promoted.ai>
2021-06-14 00:06:19 -0700

    Update message

--- BuildProject/scripts/postinstall.sh
+++ BuildProject/scripts/postinstall.sh
@@ -6,5 +6,5 @@ cp -R ../ios node_modules/@promotedai/react-native-metrics
 cp -R ../lib node_modules/@promotedai/react-native-metrics
 cp -R ../src node_modules/@promotedai/react-native-metrics
 cp ../package.json node_modules/@promotedai/react-native-metrics
-echo "$0: Overwrite package @promotedai/react-native-metrics with local copy.\n"
+echo "$0: Overwrote package @promotedai/react-native-metrics with local copy.\n"
 
