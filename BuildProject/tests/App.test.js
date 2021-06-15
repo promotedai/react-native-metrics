ccbd5bf54ab33781c2f66036b2179da1712140bf
Y Wang <limepi@gmail.com>
2021-06-15 09:41:32 -0700

    wip

--- BuildProject/tests/App.test.js
+++ BuildProject/tests/App.test.js
@@ -19,12 +19,19 @@ beforeAll(async () => {
 
 test('Test All Promoted Logging Calls', async () => {
   const testAllButton = await driver.elementByAccessibilityId('test-all-button');
-  // Need to tap twice for this to work. No comment.
-  await driver.tapElement(testAllButton);
-  await driver.tapElement(testAllButton);
   const messagesText = await driver.elementByAccessibilityId('messages-text');
-  const s = await messagesText.text();
-  if (!s.endsWith('All logging passed')) {
-    fail(s);
+  const tries = 10;
+  var i;
+  for (i = 0; i < tries; i++) {
+    await driver.sleep(1000);
+    await driver.tapElement(testAllButton);
+    const s = await messagesText.text();
+    if (!s) { continue; }  // No result yet.
+    if (s.endsWith('All logging passed')) {
+      return;  // Got the right message, test passes.
+    } else {
+      fail(s);
+    }
   }
+  fail('Test produced no output after ' + tries + ' tries');
 });
