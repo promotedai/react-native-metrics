990b320f66655539f2ffae74458acab5dcd53bf1
Y Wang <yu-hong@promoted.ai>
2021-08-14 16:54:56 -0700

    Export CollectionTrackerArgs/Props

--- src/index.tsx
+++ src/index.tsx
@@ -7,6 +7,7 @@ export default PromotedMetrics as PromotedMetricsType
 export type { AncestorIds } from './PromotedMetricsType'
 export { ActionType } from './ActionType'
 export { CollectionTracker, useCollectionTracker } from './CollectionTracker'
+export type { CollectionTrackerArgs, CollectionTrackerProps } from './CollectionTracker'
 export { ImpressionSourceType } from './ImpressionSourceType'
 export { useImpressionTracker, promotedViewabilityConfig } from './useImpressionTracker'
 export { useViewTracker } from './useViewTracker'
