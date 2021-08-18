133f0f273b995e95439f5109a74b0b607c13dcd3
Y Wang <yu-hong@promoted.ai>
2021-08-18 11:51:50 -0700

    Split test based on env variable

--- BuildProject/scripts/pretest.sh
+++ BuildProject/scripts/pretest.sh
@@ -1,39 +1,56 @@
 #!/bin/zsh
 set -e
 
+build_ios_app() {
+  echo "$0: [iOS] Building archive\n"
+  xcodebuild -workspace ios/BuildProject.xcworkspace -scheme BuildProject -archivePath build/ReactNativeMetricsBuildProject archive -sdk iphonesimulator -destination 'platform=iOS Simulator,name=iPhone 11'
+
+  echo "$0: [iOS] Copy archive to build/Payload dir\n"
+  cp -R build/ReactNativeMetricsBuildProject.xcarchive/Products/Applications/BuildProject.app build/Payload
+
+  echo "$0: [iOS] Creating IPA\n"
+  zip -r -X build/BuildProject.ipa build/Payload
+}
+
+build_android_app() {
+  echo "$0: [Android] Creating assets and res dirs\n"
+  mkdir -p android/app/src/main/assets
+  mkdir -p android/app/src/main/res
+
+  echo "$0: [Android] Bundling app\n"
+  react-native bundle --platform android --dev false --entry-file index.js --bundle-output android/app/src/main/assets/index.android.bundle --assets-dest android/app/src/main/res
+
+  echo "$0: [Android] Removing duplicate dirs\n"
+  rm -rf android/app/src/main/res/drawable-mdpi
+  rm -rf android/app/src/main/res/raw
+
+  echo "$0: [Android] Building apk\n"
+  cd android
+  ./gradlew clean
+  ./gradlew assembleRelease
+
+  echo "$0: [Android] Copy apk to build dir\n"
+  cd ..
+  cp android/app/build/outputs/apk/release/app-release.apk build/
+}
+
 echo "$0: Removing existing build dir\n"
 rm -rf build
 
 echo "$0: Creating build dir\n"
 mkdir -p build/Payload
 
-echo "$0: [iOS] Building archive\n"
-xcodebuild -workspace ios/BuildProject.xcworkspace -scheme BuildProject -archivePath build/ReactNativeMetricsBuildProject archive -sdk iphonesimulator -destination 'platform=iOS Simulator,name=iPhone 11'
-
-echo "$0: [iOS] Copy archive to build/Payload dir\n"
-cp -R build/ReactNativeMetricsBuildProject.xcarchive/Products/Applications/BuildProject.app build/Payload
-
-echo "$0: [iOS] Creating IPA\n"
-zip -r -X build/BuildProject.ipa build/Payload
-
-echo "$0: [Android] Creating assets and res dirs\n"
-mkdir -p android/app/src/main/assets
-mkdir -p android/app/src/main/res
-
-echo "$0: [Android] Bundling app\n"
-react-native bundle --platform android --dev false --entry-file index.js --bundle-output android/app/src/main/assets/index.android.bundle --assets-dest android/app/src/main/res
-
-echo "$0: [Android] Removing duplicate dirs\n"
-rm -rf android/app/src/main/res/drawable-mdpi
-rm -rf android/app/src/main/res/raw
-
-echo "$0: [Android] Building apk\n"
-cd android
-./gradlew clean
-./gradlew assembleRelease
-
-echo "$0: [Android] Copy apk to build dir\n"
-cd ..
-cp android/app/build/outputs/apk/release/app-release.apk build/
+case "$TEST_PLATFORM" in
+("ios")
+  build_ios_app
+  ;;
+("android")
+  build_android_app
+  ;;
+*)
+  echo "Unknown TEST_PLATFORM: $TEST_PLATFORM"
+  exit 1
+  ;;
+esac
 
 echo "$0: Success\n"
--- BuildProject/tests/App.test.js
+++ BuildProject/tests/App.test.js
@@ -1,65 +1,58 @@
-import wd from 'wd';
-import { retry } from './retry.js';
+import wd from 'wd'
+import { retry } from './retry.js'
 
-jasmine.DEFAULT_TIMEOUT_INTERVAL = 600000;
-const PORT = 4723;
+jasmine.DEFAULT_TIMEOUT_INTERVAL = 600000
+const TEST_PLATFORM = process.env.TEST_PLATFORM
+const PORT = 4723
 
-const iosConfig = {
-  platformName: "iOS",
-  platformVersion: "14.4",
-  deviceName: "iPhone 11",
-  app: "build/BuildProject.ipa",
-  automationName: "XCUITest",
-};
+const config =
+  (TEST_PLATFORM == 'ios') ? {
+    platformName: "iOS",
+    platformVersion: "14.4",
+    deviceName: "iPhone 11",
+    app: "build/BuildProject.ipa",
+    automationName: "XCUITest",
+  } :
+  (TEST_PLATFORM == 'android') ? {
+    platformName: "Android",
+    deviceName: "Android Emulator",
+    app: "build/app-release.apk",
+  } :
+  null
 
-const androidConfig = {
-  platformName: "Android",
-  deviceName: "Android Emulator",
-  app: "build/app-release.apk",
-};
-
-const iosDriver = wd.promiseChainRemote('localhost', PORT);
-const androidDriver = wd.promiseChainRemote('localhost', PORT);
+const driver = wd.promiseChainRemote('localhost', PORT)
 
 beforeAll(async () => {
-  await iosDriver.init(iosConfig);
-  await androidDriver.init(androidConfig);
+  if (config == null) fail(`Unknown TEST_PLATFORM: ${TEST_PLATFORM}`)
+  await driver.init(config)
 })
 
-const testPromotedLoggingCalls = async (driver) => {
+test(`Test All Promoted Logging Calls (${TEST_PLATFORM})`, async () => {
 
   class EmptyResultTextError extends Error {
     constructor() {
-      super('Test produced no result text');
-      this.name = 'EmptyResultTextError';
+      super('Test produced no result text')
+      this.name = 'EmptyResultTextError'
     }
   }
 
   await retry(async () => {
-    const messagesText = await driver.elementByAccessibilityId('messages-text');
-    const s = await messagesText.text();
+    const messagesText = await driver.elementByAccessibilityId('messages-text')
+    const s = await messagesText.text()
     // Text not yet available. Cause a retry (or failure).
-    if (!s) throw new EmptyResultTextError();
+    if (!s) throw new EmptyResultTextError()
     // Text is available. Check that it contains what we expect.
-    if (!s.endsWith('All hooks passed')) fail(s);
-  }, /*errorClassesToIgnore=*/[EmptyResultTextError], driver);
+    if (!s.endsWith('All hooks passed')) fail(s)
+  }, /*errorClassesToIgnore=*/[EmptyResultTextError], driver)
 
   await retry(async () => {
-    const testAllButton = await driver.elementByAccessibilityId('test-all-button');
-    await driver.tapElement(testAllButton);
-    const messagesText = await driver.elementByAccessibilityId('messages-text');
-    const s = await messagesText.text();
+    const testAllButton = await driver.elementByAccessibilityId('test-all-button')
+    await driver.tapElement(testAllButton)
+    const messagesText = await driver.elementByAccessibilityId('messages-text')
+    const s = await messagesText.text()
     // Text not yet available. Cause a retry (or failure).
-    if (!s) throw new EmptyResultTextError();
+    if (!s) throw new EmptyResultTextError()
     // Text is available. Check that it contains what we expect.
-    if (!s.endsWith('All logging passed')) fail(s);
-  }, /*errorClassesToIgnore=*/[EmptyResultTextError], driver);
-}
-
-test('Test All Promoted Logging Calls iOS', async () => {
-  testPromotedLoggingCalls(iosDriver)
-})
-
-test('Test All Promoted Logging Calls Android', async () => {
-  testPromotedLoggingCalls(androidDriver)
+    if (!s.endsWith('All logging passed')) fail(s)
+  }, /*errorClassesToIgnore=*/[EmptyResultTextError], driver)
 })
