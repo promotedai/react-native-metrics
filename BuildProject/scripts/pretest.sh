c5dc67f8bf6e2bfd8ddcd573d292589cce3c6fd9
Y Wang <yu-hong@promoted.ai>
2021-08-17 18:44:14 -0700

    Build Android apk for test

--- BuildProject/scripts/pretest.sh
+++ BuildProject/scripts/pretest.sh
@@ -7,13 +7,33 @@ rm -rf build
 echo "$0: Creating build dir\n"
 mkdir -p build/Payload
 
-echo "$0: Building archive\n"
+echo "$0: [iOS] Building archive\n"
 xcodebuild -workspace ios/BuildProject.xcworkspace -scheme BuildProject -archivePath build/ReactNativeMetricsBuildProject archive -sdk iphonesimulator -destination 'platform=iOS Simulator,name=iPhone 11'
 
-echo "$0: Copy archive to build/Payload\n"
+echo "$0: [iOS] Copy archive to build/Payload dir\n"
 cp -R build/ReactNativeMetricsBuildProject.xcarchive/Products/Applications/BuildProject.app build/Payload
 
-echo "$0: Creating IPA\n"
+echo "$0: [iOS] Creating IPA\n"
 zip -r -X build/BuildProject.ipa build/Payload
+
+echo "$0: [Android] Creating assets and res dirs\n"
+mkdir -p android/app/src/main/assets
+mkdir -p android/app/src/main/res
+
+echo "$0: [Android] Bundling app\n"
+react-native bundle --platform android --dev false --entry-file index.js --bundle-output android/app/src/main/assets/index.android.bundle --assets-dest android/app/src/main/res
+
+echo "$0: [Android] Removing duplicate dirs\n"
+rm -rf android/app/src/main/res/drawable-mdpi
+rm -rf android/app/src/main/res/raw
+
+echo "$0: [Android] Building apk\n"
+cd android
+./gradlew clean
+./gradlew assembleRelease
+
+echo "$0: [Android] Copy apk to build dir\n"
+cd ..
+cp android/app/build/outputs/apk/release/app-release.apk build/
 
 echo "$0: Success\n"
