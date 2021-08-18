5f1fefead99df2be0f9d4dc78c73ae18d76acfcb
Y Wang <yu-hong@promoted.ai>
2021-08-18 12:34:48 -0700

    Add accessibilityLabel. Remove semicolons.

--- BuildProject/App.tsx
+++ BuildProject/App.tsx
@@ -1,116 +1,109 @@
-/**
- * Promoted React Native Metrics testing app.
- *
- * @format
- * @flow strict-local
- */
-
-import PromotedMetrics, { ActionType, ImpressionSourceType, useImpressionTracker, useViewTracker } from '@promotedai/react-native-metrics';
-import type { AncestorIds } from '@promotedai/react-native-metrics';
-import React, { useEffect, useRef, useState } from 'react';
-import type { Node } from 'react';
+import PromotedMetrics, { ActionType, ImpressionSourceType, useImpressionTracker, useViewTracker } from '@promotedai/react-native-metrics'
+import type { AncestorIds } from '@promotedai/react-native-metrics'
+import React, { useEffect, useRef, useState } from 'react'
+import type { Node } from 'react'
 import {
   Button,
   SafeAreaView,
   StatusBar,
   Text,
   useColorScheme,
-} from 'react-native';
+} from 'react-native'
 
 import {
   Colors,
-} from 'react-native/Libraries/NewAppScreen';
+} from 'react-native/Libraries/NewAppScreen'
 
 const App: () => Node = () => {
-  const isDarkMode = useColorScheme() === 'dark';
+  const isDarkMode = useColorScheme() === 'dark'
 
   const backgroundStyle = {
     backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
+  }
 
-  const [text, setText] = useState('');
+  const [text, setText] = useState('')
 
   const handleTestAll = () => {
-    var allMessages = text + '\n';
+    var allMessages = text + '\n'
     const recordTestPassed = (message) => {
-      allMessages += 'Passed: ' + message + '\n';
-    };
+      allMessages += 'Passed: ' + message + '\n'
+    }
 
     try {
-      testStartSession(recordTestPassed);
-      testLogEvents(recordTestPassed);
-      testCollectionView(recordTestPassed);
-      testAncestorIds(recordTestPassed);
-      allMessages += 'All logging passed';
-      setText(allMessages);
+      testStartSession(recordTestPassed)
+      testLogEvents(recordTestPassed)
+      testCollectionView(recordTestPassed)
+      testAncestorIds(recordTestPassed)
+      allMessages += 'All logging passed'
+      setText(allMessages)
     } catch (err) {
-      setText(err.message);
+      setText(err.message)
     }
-  };
+  }
 
   const testStartSession = (recordTestPassed) => {
-    PromotedMetrics.startSessionAndLogUser('foobar');
-    recordTestPassed('startSessionAndLogUser');
+    PromotedMetrics.startSessionAndLogUser('foobar')
+    recordTestPassed('startSessionAndLogUser')
 
-    PromotedMetrics.startSessionAndLogSignedOutUser();
-    recordTestPassed('startSessionAndLogSignedOutUser');
+    PromotedMetrics.startSessionAndLogSignedOutUser()
+    recordTestPassed('startSessionAndLogSignedOutUser')
   }
 
   const testLogEvents = (recordTestPassed) => {
-    const content = { _id: 'foobar'};
+    const content = { _id: 'foobar'}
 
-    PromotedMetrics.logImpression(content);
-    recordTestPassed('logImpression');
+    PromotedMetrics.logImpression(content)
+    recordTestPassed('logImpression')
 
-    PromotedMetrics.logImpressionWithSourceType(content, ImpressionSourceType.Delivery);
-    recordTestPassed('logImpressionWithSourceType');
+    PromotedMetrics.logImpressionWithSourceType(content, ImpressionSourceType.Delivery)
+    recordTestPassed('logImpressionWithSourceType')
 
-    PromotedMetrics.logViewReady('foobar', 'batman');
-    recordTestPassed('logViewReady');
+    PromotedMetrics.logViewReady('foobar', 'batman')
+    recordTestPassed('logViewReady')
 
-    PromotedMetrics.logViewChange('spaghetti', 'meatballs');
-    recordTestPassed('logViewChange');
+    PromotedMetrics.logViewChange('spaghetti', 'meatballs')
+    recordTestPassed('logViewChange')
 
-    PromotedMetrics.logNavigateAction(content);
-    recordTestPassed('logNavigateAction');
+    PromotedMetrics.logNavigateAction(content)
+    recordTestPassed('logNavigateAction')
 
-    PromotedMetrics.logNavigateActionWithScreenName(content, 'screen');
-    recordTestPassed('logNavigateActionWithScreenName');
+    PromotedMetrics.logNavigateActionWithScreenName(content, 'screen')
+    recordTestPassed('logNavigateActionWithScreenName')
 
-    PromotedMetrics.logAction(ActionType.Share, content);
-    recordTestPassed('logAction');
+    PromotedMetrics.logAction(ActionType.Share, content)
+    recordTestPassed('logAction')
 
-    PromotedMetrics.logActionWithName(ActionType.Share, content, 'custom');
-    recordTestPassed('logActionWithName');
+    PromotedMetrics.logActionWithName(ActionType.Share, content, 'custom')
+    recordTestPassed('logActionWithName')
   }
 
   const testCollectionView = (recordTestPassed) => {
-    const content = { _id: 'foobar'};
+    const content = { _id: 'foobar'}
 
-    PromotedMetrics.collectionViewDidMount('hello', ImpressionSourceType.ClientBackend);
-    recordTestPassed('collectionViewDidMount');
+    PromotedMetrics.collectionViewDidMount('hello', ImpressionSourceType.ClientBackend)
+    recordTestPassed('collectionViewDidMount')
 
-    PromotedMetrics.collectionViewDidChange([], 'hello');
-    recordTestPassed('collectionViewDidChange');
+    PromotedMetrics.collectionViewDidChange([], 'hello')
+    recordTestPassed('collectionViewDidChange')
 
-    PromotedMetrics.collectionViewActionDidOccur(ActionType.AddToCart, content, 'hello');
-    recordTestPassed('collectionViewActionDidOccur');
+    PromotedMetrics.collectionViewActionDidOccur(ActionType.AddToCart, content, 'hello')
+    recordTestPassed('collectionViewActionDidOccur')
 
-    PromotedMetrics.collectionViewWillUnmount('hello');
-    recordTestPassed('collectionViewWillUnmount');
+    PromotedMetrics.collectionViewWillUnmount('hello')
+    recordTestPassed('collectionViewWillUnmount')
   }
 
   const testAncestorIds = (recordTestPassed) => {
-    PromotedMetrics.getCurrentOrPendingAncestorIds();
-    recordTestPassed('getCurrentOrPendingAncestorIds');
+    PromotedMetrics.getCurrentOrPendingAncestorIds()
+    recordTestPassed('getCurrentOrPendingAncestorIds')
 
     const ancestorIds = {
       logUserId: 'batman',
       sessionId: 'gotham',
       viewId: 'joker'
-    } as AncestorIds;
-    PromotedMetrics.setAncestorIds(ancestorIds);
-    recordTestPassed('setAncestorIds');
+    } as AncestorIds
+    PromotedMetrics.setAncestorIds(ancestorIds)
+    recordTestPassed('setAncestorIds')
   }
 
   try {
@@ -122,29 +115,34 @@ const App: () => Node = () => {
       }),
       'TestCollectionViewName',
       ImpressionSourceType.Delivery
-    );
-    useViewTracker(useRef());
+    )
+    useViewTracker(useRef())
     useEffect(() => {
       setText('Passed: useImpressionTracker\n' +
               'Passed: useViewTracker\n' +
-              'All hooks passed');
-    }, []);
+              'All hooks passed')
+    }, [])
   } catch (err) {
     useEffect(() => {
-      setText(err.message);
-    }, []);
+      setText(err.message)
+    }, [])
   }
 
   return (
     <SafeAreaView style={backgroundStyle}>
       <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
       <Button
-        title='Test All'
+        accessibilityLabel='test-all-button'
         onPress={handleTestAll}
-        testID='test-all-button'/>
-      <Text testID='messages-text'>{text}</Text>
+        testID='test-all-button'
+        title='Test All'/>
+      <Text
+        accessibilityLabel='messages-text'
+        testID='messages-text'>
+        {text}
+      </Text>
     </SafeAreaView>
-  );
-};
+  )
+}
 
-export default App;
+export default App
