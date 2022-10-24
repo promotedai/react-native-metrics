cf5347848d9008ac020b325452aa63c90d041caf
Yuna Wang <yuna@promoted.ai>
2022-10-24 11:48:27 -0700

    Add error handling flag (#90)

--- ios/ReactNativeError.swift
+++ ios/ReactNativeError.swift
@@ -15,13 +15,13 @@ extension ReactNativeError: NSErrorProperties {
   }
 }
 
-#if DEBUG
+#if DEBUG || PROMOTED_ERROR_HANDLING
 extension ReactNativeError: ErrorDetails {
   var details: String {
     switch self {
     case .moduleNotInitialized:
       return """
-      PromotedMetricsModule was not initialized correctly. This may be due to a recent change to AppDelegate. Make sure that PromotedMetricsModule is included in -extraModulesForBridge:.
+      PromotedMetricsModule was not initialized correctly. This may be due to a recent change to AppDelegate. Make sure that PromotedMetricsModule is included in -[AppDelegate extraModulesForBridge:].
       """
     }
   }
